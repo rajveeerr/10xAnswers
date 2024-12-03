b10697cdb427cca26a0cd5793a7468e192eb4cda
rajveeerr <rajveergreets@gmail.com>
2024-12-03 15:58:50 +0530

    removed unnessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { RecoilRoot } from 'recoil';
 import './App.css'
-import ChatBot from './components/chatBot'
-import HomePage from './pages/homePage';
-import CustomizationPage from './pages/customizationPaje';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Layout from './pages/layout';
+import {ChatBot} from './components/chatBot'
 
 
 function App() {
-  const allRoutes=[{
-    path: "/",
-    element: <HomePage/>
-  },{
-    path: "/customize",
-    element: <CustomizationPage/>
-  }]
 
   return (
     <>
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout/>}>
-            {allRoutes.map((currentRoute,index)=><Route key={index} path={currentRoute.path} element={currentRoute.element}></Route>)}
-          </Route>
-        </Routes>
-      </BrowserRouter>      
-      {/* <ChatBot/> */}
-    </RecoilRoot>
+       <ChatBot //avoid adding position to the chatbot through style or class it will result in abnormal behavior
+          chatWindowStyle={{backgroundColor:"rgb(11 10 10)"}} 
+          chatComponentStyle={{maxHeight:"580px",height:"auto",width:"400px",margin:0}}    
+          chatComponentClassName="static md:absolute"
+          botIcon="./logoImg2.jpg" 
+          userIcon= "./logoImg.jpg"
+          stylizeTitle={{emphasized:"10x",normal:"Answers"}}//if emphasized title and title both are given priority will be given to emp title
+          geminiApi="AIzaSyDpdTZc3GzpvmAfjwfwE14BoQaTu4QmMo0"
+          // x={500} 
+          // y={625} 
+          backendUrl="https://ask-10x-questions.vercel.app/" 
+          // title="Why Ask?"
+          // prompt="You are an artist" 
+          draggable={false} 
+          // description="Why not ask you questions?" 
+          cta="Start Asking"
+          startOpen={true}
+        /> 
     </>)
 }
 
